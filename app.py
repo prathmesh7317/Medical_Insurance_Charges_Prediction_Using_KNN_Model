b9d52bda2941f228f06a2d95f3d16bcd8de1bffc
prathmesh7317 <prathmesh7317@gmail.com>
2024-12-05 17:34:01 +0530

    added app file

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def get_medical():
         # return jsonify({"predicted_charges": CHARGES}) # For POSTMAN Only
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8080)
